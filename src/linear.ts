dfbe5db1e731bb221cdc98aeff5eb71e2ced8b7d
Andrea Franz <andrea@gravityblast.com>
2023-04-18 15:53:55 +0200

    add matching cap option

--- src/linear.ts
+++ src/linear.ts
@@ -19,11 +19,14 @@ type AggregatedContributions = {
 export type LinearQFOptions = {
   minimumAmount: number;
   ignoreSaturation: boolean;
+  matchingCapAmount: number | undefined; // an amount, not a percentage
 };
 
 export type Calculation = {
   totalReceived: number;
   sumOfSqrt: number;
+  capOverflow: number;
+  matchedWithoutCap: number;
   matched: number;
 };
 
@@ -34,11 +37,14 @@ export type RecipientsCalculations = {
 const defaultLinearQFOptions = (): LinearQFOptions => ({
   minimumAmount: 0,
   ignoreSaturation: false,
+  matchingCapAmount: undefined,
 });
 
 const newCalculation = (totalReceived: number): Calculation => ({
   totalReceived,
   sumOfSqrt: 0,
+  capOverflow: 0,
+  matchedWithoutCap: 0,
   matched: 0,
 });
 
@@ -86,7 +92,9 @@ export const linearQF = (
 
   let totSqrtSum = 0;
 
-  // for each recipient
+  // for each recipient in aggregated.list we calculate the sum
+  // of sqrt in the calculations object,
+  // and we sum all the sqrt in totSqrtSum
   for (const recipient in aggregated.list) {
     let totRecipientSqrtSum = 0;
     // for each recipient contribution aggregated by contributor
@@ -106,8 +114,13 @@ export const linearQF = (
       Math.pow(totRecipientSqrtSum, 2) - calculations[recipient].totalReceived;
   }
 
-  // for each recipient
-  for (const recipient in aggregated.list) {
+  let totalCapOverflow = 0;
+  let totalUnderCap = 0;
+
+  // for each recipient in calculations
+  // we calculate the final match based on
+  // its sqrt and the totSqrtSum
+  for (const recipient in calculations) {
     const val =
       Math.pow(calculations[recipient].sumOfSqrt, 2) -
       calculations[recipient].totalReceived;
@@ -121,8 +134,42 @@ export const linearQF = (
       matchRatio = aggregated.totalReceived / matchAmount;
     }
 
-    calculations[recipient].matched =
-      ((val * matchAmount) / totSqrtSum) * matchRatio;
+    let match = ((val * matchAmount) / totSqrtSum) * matchRatio;
+    let matchWithoutCap = match;
+    let capOverflow = 0;
+
+    if (options.matchingCapAmount !== undefined) {
+      // negative if lower than the cap
+      capOverflow = match - options.matchingCapAmount;
+
+      if (capOverflow > 0) {
+        match = options.matchingCapAmount;
+      } else {
+        totalUnderCap -= capOverflow;
+      }
+    }
+
+    calculations[recipient].matchedWithoutCap = matchWithoutCap;
+    calculations[recipient].matched = match;
+    calculations[recipient].capOverflow = capOverflow;
+
+    if (capOverflow > 0) {
+      totalCapOverflow += capOverflow;
+    }
+  }
+
+  if (options.matchingCapAmount !== undefined && totalCapOverflow > 0) {
+    // redistribute the totalCapOverflow to all
+    for (const recipient in calculations) {
+      const recipientOverflow = calculations[recipient].capOverflow;
+      if (recipientOverflow < 0) {
+        // recipientOverflow is negative so we can distribute something
+        // to the current recipient
+        const maxAmountToCap = -recipientOverflow;
+        const newMatch = (maxAmountToCap * totalUnderCap) / totalUnderCap;
+        calculations[recipient].matched = newMatch;
+      }
+    }
   }
 
   return calculations;
--- src/test/linear.test.ts
+++ src/test/linear.test.ts
@@ -104,25 +104,32 @@ describe("linearQF", () => {
       const res = linearQF(contributions, matchAmount, {
         minimumAmount: 0,
         ignoreSaturation: true,
+        matchingCapAmount: undefined,
       });
 
       expect(Object.keys(res).length).toEqual(3);
 
       expect(res["project_1"]).toEqual({
+        capOverflow: 0,
         sumOfSqrt: 7,
         totalReceived: 15,
+        matchedWithoutCap: 13.6,
         matched: 13.6,
       });
 
       expect(res["project_2"]).toEqual({
+        capOverflow: 0,
         sumOfSqrt: 8,
         totalReceived: 10,
+        matchedWithoutCap: 21.6,
         matched: 21.6,
       });
 
       expect(res["project_3"]).toEqual({
+        capOverflow: 0,
         sumOfSqrt: 14,
         totalReceived: 34,
+        matchedWithoutCap: 64.8,
         matched: 64.8,
       });
     });
@@ -146,6 +153,7 @@ describe("linearQF", () => {
       const res = linearQF(contributionsWithLowAmounts, matchAmount, {
         minimumAmount: 1,
         ignoreSaturation: true,
+        matchingCapAmount: undefined,
       });
 
       expect(Object.keys(res).length).toEqual(3);
@@ -159,20 +167,26 @@ describe("linearQF", () => {
       expect(Object.keys(res).length).toEqual(3);
 
       expect(res["project_1"]).toEqual({
+        capOverflow: 0,
         sumOfSqrt: 7,
         totalReceived: 15,
+        matchedWithoutCap: 1.36,
         matched: 1.36,
       });
 
       expect(res["project_2"]).toEqual({
+        capOverflow: 0,
         sumOfSqrt: 8,
         totalReceived: 10,
+        matchedWithoutCap: 2.16,
         matched: 2.16,
       });
 
       expect(res["project_3"]).toEqual({
+        capOverflow: 0,
         sumOfSqrt: 14,
         totalReceived: 34,
+        matchedWithoutCap: 6.48,
         matched: 6.48,
       });
     });
@@ -217,15 +231,55 @@ describe("linearQF", () => {
       expect(Object.keys(res).length).toEqual(2);
 
       expect(res["project_1"]).toEqual({
+        capOverflow: 0,
         sumOfSqrt: 8.94427190999916,
         totalReceived: 20,
+        matchedWithoutCap: 36,
         matched: 36,
       });
 
       expect(res["project_2"]).toEqual({
+        capOverflow: 0,
         sumOfSqrt: 8.94427190999916,
         totalReceived: 40,
+        matchedWithoutCap: 24,
         matched: 24,
+      });
+    });
+
+    test("calculates the matches with matching cap", async () => {
+      const matchAmount = 100;
+      const res = linearQF(contributions, matchAmount, {
+        minimumAmount: 0,
+        ignoreSaturation: true,
+        matchingCapAmount: 50,
+      });
+
+      expect(Object.keys(res).length).toEqual(3);
+
+      expect(res["project_1"]).toEqual({
+        capOverflow: -36.4,
+        sumOfSqrt: 7,
+        totalReceived: 15,
+        matchedWithoutCap: 13.6,
+        matched: 36.4,
+      });
+
+      expect(res["project_2"]).toEqual({
+        capOverflow: -28.4,
+        sumOfSqrt: 8,
+        totalReceived: 10,
+        matchedWithoutCap: 21.6,
+        matched: 28.4,
+      });
+
+      expect(res["project_3"]).toEqual({
+        // we will change this test in the next PRs using BigInt and cents or token decimals
+        capOverflow: 14.799999999999997,
+        sumOfSqrt: 14,
+        totalReceived: 34,
+        matchedWithoutCap: 64.8,
+        matched: 50,
       });
     });
   });
